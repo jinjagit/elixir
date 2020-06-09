719a3ff64d75b76ff6b332c1be0fdbc12107062d
Simon Tharby <jinjamail@gmail.com>
2020-06-09 21:31:34 +0100

    more stuff

--- map.ex
+++ map.ex
@@ -1,17 +1,17 @@
 car = %{make: "Ford", type: "coupe", cc: 2000}
 
-IO.puts(car.cc)
+IO.puts(car.cc) #=> 2000
 
 new_car = %{car | cc: 2600}
 
-IO.puts(new_car.cc)
+IO.puts(new_car.cc) #=> 2600
 
 car = %{car | cc: 3000}
 
-IO.puts(car.cc)
+IO.puts(car.cc) #=> 3000
 
 car = Map.put(car, :color, "blue")
 
-IO.puts(car.color)
+IO.puts(car.color) #=> blue
 
-IO.inspect(car)
+IO.inspect(car) #=> %{cc: 3000, color: "blue", make: "Ford", type: "coupe"}
