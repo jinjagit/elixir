2d9819d48f7a4fe9e63bc856f40b262fc842b95b
Simon Tharby <jinjamail@gmail.com>
2020-06-12 20:36:40 +0100

    more stuff

---
+++ chapter_4_data_abstractions/simple_todo.ex
@@ -0,0 +1,29 @@
+# Note: Book uses HasDict in a basic example, but HashDict is deprecated in the
+# elixir version I am using. I will use Map instead.
+
+defmodule TodoList do
+  def new, do: Map.new
+
+  def add_entry(todo_list, date, title) do
+    Map.update(
+      todo_list,
+      date,
+      [title], # initial value
+      fn(titles)-> [title | titles] end # updater lamda
+    )
+  end
+
+  def entries(todo_list, date) do
+    Map.get(todo_list, date, [])
+  end
+end
+
+todo_list =
+  TodoList.new
+  |> TodoList.add_entry({2020, 6, 20}, "Dentist")
+  |> TodoList.add_entry({2020, 6, 21}, "Shopping")
+  |> TodoList.add_entry({2020, 6, 20}, "Movies")
+
+IO.inspect(
+  TodoList.entries(todo_list, {2020, 6, 20})
+) #=> ["Movies", "Dentist"]
