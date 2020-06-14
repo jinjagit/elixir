45c9fa1c616b53858218fcf67d3e119a217228c6
Simon Tharby <jinjamail@gmail.com>
2020-06-14 18:57:35 +0100

    more stuff

--- chapter_4_data_abstractions/protocols.ex
+++ chapter_4_data_abstractions/protocols.ex
@@ -42,3 +42,75 @@ IO.puts(todo_list) #=> #TodoList
 # protocol for a type even if you can’t modify the type’s source code.
 # You can place the protocol implementation anywhere in your own code, and the
 # runtime will be able to take advantage of it.
+
+# a protocol dispatch may in some cases be significantly slower than a direct
+# function call. A protocol consolidation will speed things up (Chapter 14).
+
+# See 4.3.3 for information on built-in protocols, (e.g. Access, Enumerable),
+# and some advice on how to implement for non-standard structures (e.g. structs)
+
+
+# Collectable to-do list:
+
+# A collectable structure is one that you can repeatedly add elements to.
+# A collectable can be used with comprehensions to collect results or with
+# Enum.into/2 to transfer elements of one structure (enumerable) to another
+# (collectable).
+
+# Making TodoList abstraction a collectable, by implementing Collectable for it:
+
+defimpl Collectable, for: TodoList do
+  def into(original) do # returns the appender lambda
+    {original, &into_callback/2}
+  end
+
+  # appender implementation (all 3 defp functions)
+  defp into_callback(todo_list, {:cont, entry}) do
+    TodoList.add_entry(todo_list, entry)
+  end
+
+  defp into_callback(todo_list, :done), do: todo_list
+  defp into_callback(todo_list, :halt), do: :ok
+end
+
+# The exported function into/1 is called by the generic code (comprehensions,
+# for example). Here you provide the implementation that returns the appender
+# lambda. This appender lambda is then repeatedly invoked by the generic code
+# to append each element to your data structure.
+# The appender function receives a to-do list and an instruction hint. If you
+# receive {:cont, entry}, then you must add a new entry. If you receive :done,
+# then you return the list, which at this point contains all appended elements.
+# Finally, :halt indicates that the operation has been canceled, and the return
+# value is ignored.
+
+imported_data = TodoList.CsvImporter.parse!("todos.csv")
+
+IO.inspect(imported_data)
+#=> [
+#=>  %{date: {2020, 6, 20}, title: "Dentist"},
+#=>  %{date: {2020, 6, 19}, title: "Shopping"},
+#=>  %{date: {2020, 6, 20}, title: "Movies"}
+#=> ]
+
+todo_list = for entry <- imported_data, into: TodoList.new, do: entry
+IO.inspect(todo_list)
+#=> %TodoList{
+#=>   auto_id: 4,
+#=>   entries: %{
+#=>     1 => %{date: {2020, 6, 20}, id: 1, title: "Dentist"},
+#=>     2 => %{date: {2020, 6, 19}, id: 2, title: "Shopping"},
+#=>     3 => %{date: {2020, 6, 20}, id: 3, title: "Movies"}
+#=>   }
+#=> }
+
+# By implementing the Collectable protocol, you essentially adapt the TodoList
+# abstraction to any generic code that relies on the that protocol, such as
+# comprehensions and Enum.into/2.
+# Note that this does not mean that a call to Enum will now work, for example,
+# since an implementation of Enumerable for TodoList has not been implemented.
+
+# list = Enum.into(todo_list, []) #=> ** (Protocol.UndefinedError) protocol Enumerable not implemented for %TodoList{...}
+
+# I have tried implementing the Enumerable protocol for TodoList, but this
+# attempt has revealed I need to learn much more about how protocols are
+# implemented, and how to understand documentation, before I can do this.
